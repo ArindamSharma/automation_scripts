cb99a1e734c23591585069a76281632c99a5772f
ArindamSharma <36325272+ArindamSharma@users.noreply.github.com>
2020-02-24 07:49:35 +0530

    Graphic Card Turn off

---
+++ radeon_off.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#disabling the Graphic Card
+cd /etc/default
+file=/etc/default/grub
+var=$(sed -n 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX=""/p' $file)
+if [ ${#var} == 0 ] ; then
+	echo "Its already changed to"
+else 
+	echo "The it has to be changed"
+	echo 'Changing GRUB_CMDLINE_LINUX="" to GRUB_CMDLINE_LINUX="radeon.modset=0"'
+	sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modset=0"/p' $file
+	echo "Changes made"
+	echo "Result will be seen after Reboot "
+fi
+#sudo sed 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modset=0"'
