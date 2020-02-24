0dd982af64e0bfb4da54e58dc3cdb095c9f2549d
ArindamSharma <36325272+ArindamSharma@users.noreply.github.com>
2020-02-24 08:01:55 +0530

    turns on Graphic Card radeon

---
+++ radeon_on.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#disabling the Graphic Card
+cd /etc/default
+file=/etc/default/grub
+var=$(sed -n 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX=""/p' $file)
+if [ ${#var} != 0 ] ; then
+	echo "Radeon already on"
+else 
+	echo "it has to be changed"
+	echo 'Changing '$(sed -n '/GRUB_CMDLINE_LINUX="radeon.modeset=0"/p' $file)' to GRUB_CMDLINE_LINUX=""'
+	sudo sed -i 's/GRUB_CMDLINE_LINUX=".*/"/p' $file
+	echo "Changes made"
+	echo "Result will be seen after Reboot "
+fi
+#sudo sed 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modset=0"'
