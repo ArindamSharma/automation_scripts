b1cbca242c371c718ac147ef0c20441ee0d2cde6
ArindamSharma <36325272+ArindamSharma@users.noreply.github.com>
2020-02-27 07:23:39 +0530

    Update radeon_off.sh

--- radeon_off.sh
+++ radeon_off.sh
@@ -8,8 +8,9 @@ if [ ${#var} == 0 ] ; then
 else 
 	echo "The it has to be changed"
 	echo 'Changing GRUB_CMDLINE_LINUX="" to GRUB_CMDLINE_LINUX="radeon.modeset=0"'
-	sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modeset=0"/p' $file
+	sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modeset=0"/g' $file
 	echo "Changes made"
+	sudo update-grub
 	echo "Result will be seen after Reboot "
 fi
 #sudo sed 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="radeon.modset=0"'
